0490019ed38910cdacc0ff65acbc99eade8fdd70
Luke Todd <luke.todd22@yahoo.com>
2019-02-06 23:29:10 -0700

    added css

--- apps/apps.js
+++ apps/apps.js
@@ -1,3 +1,16 @@
+let things = {
+  rock: {
+    img: '/assets/photos/rock-dude.jpg'
+  },
+  paper: {
+    img: '/assets/photos/paper-smiley.jpg'
+  },
+  scissors: {
+    img: '/assets/photos/scissors-happy.png'
+  }
+}
+
+
 
 let options = {
   choice1: 'Rock',
@@ -13,6 +26,13 @@ function random() {
   return options[choice];
 }
 
+// function displayCompImg() {
+//   let result = src=""
+//   let compChoice = random()
+//   if (compChoice == options.choice1) { result = things.rock.img }
+//   else if (compChoice == options.choice2) { result = things.paper.img }
+//   else result = things.scissors.img
+// }
 
 
 
@@ -20,18 +40,11 @@ function play(playerChoice) {
   let result = ''
   let compChoice = random()
   console.log(compChoice)
+  document.getElementById('compresult').innerText = compChoice
   if (playerChoice == compChoice) { result = "Draw" }
   else if (playerChoice == options.choice1 && compChoice == options.choice2) { result = "Loser!" }
   else if (playerChoice == options.choice2 && compChoice == options.choice3) { result = "Loser!" }
   else if (playerChoice == options.choice3 && compChoice == options.choice1) { result = "Loser!" }
   else result = "WINNER!!!!"
-
   document.getElementById('rockbtn').innerText = result
 }
-
-
-
-
-
-
-
